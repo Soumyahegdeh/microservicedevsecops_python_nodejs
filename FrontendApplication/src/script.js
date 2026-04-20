21de230f97da79ea4449e9d60b88d2073d69a69c
soumya <soumyahegde94@gmail.com>
2026-04-20 10:56:23 +0530

    fixed container runtime issue

--- FrontendApplication/src/script.js
+++ FrontendApplication/src/script.js
@@ -1,4 +1,4 @@
-var api = process.env.API_GATEWAY // get the API Gateway from the environment
+var api = "http://localhost:3000" // get the API Gateway from the environment
 
 $(document).ready(function() {
     $("#btn").click(function() {
