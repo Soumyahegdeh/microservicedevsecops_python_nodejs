e0282c96371d743d0b4c8d2f4b7e213d6e87055e
soumya <soumyahegde94@gmail.com>
2026-05-20 22:29:30 +0530

    fix for quoteservice health point issue

--- QuoteService/main.py
+++ QuoteService/main.py
@@ -2,16 +2,14 @@ from flask import Flask
 from flask import jsonify
 import random
 
-QUOTES_FILE = "./quotes.txt" # quote file
-quotes = [] # stores all quotes
+QUOTES_FILE = "./quotes.txt"
+quotes = []
 
-# a quote
 class Quote(object):
     def __init__(self, quote, by):
         self.quote = quote
         self.by = by
 
-# Loads quotes from a file
 def loadQuotes():
     with open(QUOTES_FILE) as file:
         lines = file.readlines()
@@ -22,22 +20,19 @@ def loadQuotes():
             
 app = Flask(__name__)
 
-# Gets a random quote 
+@app.route("/health")
+def health():
+    return jsonify({"status": "healthy"}), 200
+
 @app.route("/api/quote")
 def quote():
-    q = random.choice(quotes) # selects a random quote from file
-    return jsonify({"quote": q.quote, "by": q.by}) # return a quote
+    q = random.choice(quotes)
+    return jsonify({"quote": q.quote, "by": q.by})
 
-# 404 Erorr for unknown routes
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify({"message": "Resource not found"}), 404
 
 if __name__ == '__main__':
-    loadQuotes() # load quotes 
-    app.run(host='0.0.0.0', port=5000, debug=True) # run application
-    
-# Add this route to app.py
-@app.route("/health")
-def health():
-    return jsonify({"status": "healthy"}), 200
+    loadQuotes()
+    app.run(host='0.0.0.0', port=5000, debug=True) 
